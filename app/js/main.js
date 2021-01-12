03d598bb3ed41a18fd7ebd68111800d9d1cef17d
AleksandrVelychko <oleksandr.velychko96@gmail.com>
2021-01-12 21:12:17 +0200

    'initial'

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
+new Swiper('.slider__items', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 1.5,
+  spaceBetween: 45,
+  loop: true, 
+  freeMode: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteractive: false
+  },
+  speed: 800,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 100
+    }
+  }
+});
+
 (function($){
 	$(document).ready(function() {
 		// Code
