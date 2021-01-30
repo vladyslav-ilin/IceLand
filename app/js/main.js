032e49a13846b7fb9411f1790a6381afc69abe8d
AleksandrVelychko <oleksandr.velychko96@gmail.com>
2021-01-30 12:12:37 +0200

    road contacts js

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,84 @@
 'use strict';
 
+function initMap() {
+  let map, coords, styles, marker, info, content;
+
+  coords = {
+    lat: 64.14311360365512,
+    lng: -21.926393842068492
+  }
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: coords,
+    zoom: 13,
+    styles: styles,
+    disableDefaultUI: true,
+    zoomControl: false,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.LEFT_BOTTOM,
+    },
+    streetViewControl: true,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      mapTypeIds: ["roadmap", "satellite"],
+    },
+
+  });
+
+  marker = new google.maps.Marker({
+    position: coords,
+    map: map,
+    title: "This is here!",
+    animation: google.maps.Animation.DROP,
+    // draggable: true,
+    icon: 'images/marker.png',
+  });
+
+  content = `<h1 class="map__title">Hello</h1>
+				<p class="map__txt">This is your place!</p>`;
+
+  info = new google.maps.InfoWindow({
+    content: content,
+  });
+
+  marker.addListener("click", () => {
+    info.open(map, marker);
+  });
+}
+
 new Swiper('.slider__items', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  slidesPerView: 1.5,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  clickable: true,
+  slidesPerView: 1,
   spaceBetween: 45,
   loop: true,
-  freeMode: true,
-  autoplay: {
-    delay: 1500,
-    disableOnInteractive: false
-  },
+  freeMode: false,
+
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteractive: false
+  // },
   speed: 800,
   breakpoints: {
     768: {
       slidesPerView: 3,
-      spaceBetween: 100
+      spaceBetween: 100,
+      width: 970
+    },
+
+    480: {
+      slidesPerView: 1
     }
   }
+
 });
 
 new Swiper('.swiper__container', {
@@ -67,6 +126,8 @@ new Swiper('.swiper__container', {
       prevArrow: '<div class="booking__arrow booking__prev"><i class="icon-arrow"></div>',
       nextArrow: '<div class="booking__arrow booking__next"><i class="icon-arrow"></div>'
     });
+
+    AOS.init();
   });
 
   $(window).scroll(() => {
